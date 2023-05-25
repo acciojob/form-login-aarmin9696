3444ec1840713ca6bfef00db866a8c6b018b40ed
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-25 12:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+let fn = document.getElementsByName("fname")[0].value;
+  let ln = document.getElementsByName("lname")[0].value;
+  alert(fn + " " + ln);
+	
 
 }
